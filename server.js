354ae4af3fa416b7d32854d1cc8bc3d2c42da7f5
Claude Mopeno-Bia <mopenobiaclaude@gmail.com>
2023-10-27 10:23:25 +0100

    Update server.js

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ var corsOptions = {
     "http://192.168.100.22:3000",
     "http://192.168.100.24:3000",
     "http://192.168.100.50:3000",
-    "https://frent-end.vercel.app/",
+    "https://frent-end.vercel.app",
  ],
 
 
