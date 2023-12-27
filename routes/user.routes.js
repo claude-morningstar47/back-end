82544c8f2c5d8f6eaf36b272238155ef30f119c2
Claude Mopeno-Bia <mopenobiaclaude@gmail.com>
2023-12-27 09:48:16 +0100

    Update user.routes.js

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -7,13 +7,9 @@ import express from "express";
 const router = express.Router();
 
 router.post("/", [verifySignUp.checkDuplicateUserOrEmail, verifySignUp.checkRolesExisted], controller.createUser);
-
 router.get("/:id", [authJwt.verifyToken], controller.getUserById);
-
 router.get("/", [authJwt.verifyToken, authJwt.isAdmin], controller.getAllUsers);
-
 router.put("/:id", [authJwt.verifyToken, authJwt.isAdmin], controller.updateUser);
-
 router.delete("/:id", [authJwt.verifyToken, authJwt.isAdmin], controller.deleteUser);
 
 export default router;
