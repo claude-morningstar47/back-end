901782a26a02757a490e19c01c0efd9b3839f741
Claude Mopeno-Bia <mopenobiaclaude@gmail.com>
2023-12-28 08:48:34 +0100

    m-a-j
    mise a jour des fichiers de journalisations.

--- config/db.config.js
+++ config/db.config.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { db } from "../models/index.js";
 
 const connectDB = (url) => {
   return mongoose.connect(url, {
@@ -6,4 +7,24 @@ const connectDB = (url) => {
     useUnifiedTopogy: true,
   });
 };
-export default connectDB;
+// Connexion à la base de données MongoDB
+const Role = db.role;
+// Initialisation des rôles dans la base de données
+async function initial() {
+  try {
+    const count = await Role.estimatedDocumentCount();
+    if (count === 0) {
+      await Promise.all([
+        new Role({ name: "user" }).save(),
+        new Role({ name: "moderator" }).save(),
+        new Role({ name: "admin" }).save(),
+      ]);
+      console.log("Roles added to the collection.");
+    }
+  } catch (err) {
+    // logger.error("Error initializing roles", err);
+    console.error("Error initializing roles", err);
+  }
+}
+
+export { connectDB, initial };
