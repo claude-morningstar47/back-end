3b0255e8e330f8c87679af772e789ccb065fc667
Claude Mopeno-Bia <mopenobiaclaude@gmail.com>
2023-12-28 09:21:03 +0100

    test de stationnement

--- background.js
+++ background.js
@@ -1,13 +1,13 @@
 // background.js
-import { db } from "./models/index.js";
 import RefreshToken from "./models/refreshToken.model.js";
 
-export default async function removeExpiredRefreshTokens() {
+async function removeExpiredRefreshTokens() {
   try {
     // Supprimez les refresh tokens expirés
-    const result = await RefreshToken.removeExpiredTokens();
-    console.log(`Deleted ${result.deletedCount} expired refresh tokens.`);
+    await RefreshToken.removeExpiredTokens();
+    console.log(`Deleted expired refresh tokens.`);
   } catch (err) {
     console.error("Error deleting expired refresh tokens", err);
   }
 }
+export { removeExpiredRefreshTokens };
--- server.js
+++ server.js
@@ -10,39 +10,56 @@ import { db } from "./models/index.js";
 import { _config } from "./config/global.config.js";
 import { serverConfig } from "./config/server.config.mjs";
 import router from "./routes/index.js";
-import winston from 'winston';
-import fs from 'fs';
+import winston from "winston";
+import fs from "fs";
+import { initial } from "./config/db.config.js";
+import { removeExpiredRefreshTokens } from "./background.js";
 
 const app = express();
+let logger;
 
-// Configuration des répertoires et fichiers de journal
-// const logDirectory = path.join(path.dirname(fileURLToPath(import.meta.url)), 'logs');
-// const logFilePath = path.join(logDirectory, 'access.log');
-// const errFilePath = path.join(logDirectory, 'error.log');
+if (serverConfig.environment !== "production") {
+  // Configuration des répertoires et fichiers de journal
+  const logDirectory = path.join(
+    path.dirname(fileURLToPath(import.meta.url)),
+    "logs"
+  );
+  const logFilePath = path.join(logDirectory, "access.log");
+  const errFilePath = path.join(logDirectory, "error.log");
 
-// Vérification et création du répertoire des journaux
-// if (!fs.existsSync(logDirectory)) {
-//   fs.mkdirSync(logDirectory);
-// }
-
-// Adaptateur pour le flux de Winston
-class WinstonStreamAdapter {
-  write(message) {
-    logger.info(message);
+  // Vérification et création du répertoire des journaux
+  if (!fs.existsSync(logDirectory)) {
+    fs.mkdirSync(logDirectory);
   }
+
+  // Adaptateur pour le flux de Winston
+  class WinstonStreamAdapter {
+    write(message) {
+      logger.info(message);
+    }
+  }
+
+  //Configuration du logger Winston
+  logger = winston.createLogger({
+    level: "info",
+    format: winston.format.json(),
+    transports: [
+      new winston.transports.Console(),
+      new winston.transports.File({ filename: errFilePath, level: "error" }),
+      new winston.transports.File({ filename: logFilePath, level: "info" }),
+    ],
+  });
+
+  // Utilisation du logger Winston avec Morgan pour la journalisation des requêtes
+  app.use(morgan("combined", { stream: new WinstonStreamAdapter() }));
+
+  // Utilisation de setTimeout pour déclencher removeExpiredRefreshTokens après 20 secondes
+  setTimeout(() => {
+    removeExpiredRefreshTokens();
+  }, 20000);
+} else {
+  app.use(morgan("dev"));
 }
-
-// Configuration du logger Winston
-// const logger = winston.createLogger({
-//   level: 'info',
-//   format: winston.format.json(),
-//   transports: [
-//     new winston.transports.Console(),
-//     new winston.transports.File({ filename: errFilePath, level: 'error' }),
-//     new winston.transports.File({ filename: logFilePath, level: 'info' }),
-//   ],
-// });
-
 // Configuration des middlewares
 app.use(session(serverConfig.sessionOptions));
 app.use(cors(serverConfig.corsOptions));
@@ -54,10 +71,6 @@ const __dirname = path.dirname(__filename);
 const faviconPath = path.join(__dirname, "appointment.png");
 app.use(favicon(faviconPath));
 
-// Utilisation du logger Winston avec Morgan pour la journalisation des requêtes
-// app.use(morgan("combined", { stream: new WinstonStreamAdapter() }));
-app.use(morgan('dev'));
-
 // Configuration du body parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -67,18 +80,17 @@ app.use("/", router);
 
 // Gestionnaire d'erreurs pour les erreurs internes du serveur (500)
 app.use((err, req, res, next) => {
-  // logger.error(err.stack);
+  logger.error(err.stack);
   console.error(err.stack);
-  res.status(err.status || 500).json({ message: err.message || "Internal Server Error" });
+  res
+    .status(err.status || 500)
+    .json({ message: err.message || "Internal Server Error" });
 });
 
 // Démarrage du serveur
 app.listen(serverConfig.port, () => {
   console.log(`Server is running on port ${serverConfig.port}.`);
 });
-
-// Connexion à la base de données MongoDB
-const Role = db.role;
 
 // Fonction pour la connexion à la base de données et initialisation
 async function connectToDb() {
@@ -87,7 +99,7 @@ async function connectToDb() {
     console.log("Successfully connected to MongoDB.");
     await initial();
   } catch (error) {
-    // logger.error("Connection", error);
+    logger.error("Connection", error);
     console.error("Connection", error);
     process.exit(1);
   }
@@ -95,21 +107,3 @@ async function connectToDb() {
 
 // Appel de la fonction de connexion à la base de données
 connectToDb();
-
-// Initialisation des rôles dans la base de données
-async function initial() {
-  try {
-    const count = await Role.estimatedDocumentCount();
-    if (count === 0) {
-      await Promise.all([
-        new Role({ name: "user" }).save(),
-        new Role({ name: "moderator" }).save(),
-        new Role({ name: "admin" }).save(),
-      ]);
-      console.log("Roles added to the collection.");
-    }
-  } catch (err) {
-    // logger.error("Error initializing roles", err);
-    console.error("Error initializing roles", err);
-  }
-}
